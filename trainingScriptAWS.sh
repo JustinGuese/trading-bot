ff510f1e0b3c007bc948d9f6fa969d264424344e
EC2 Default User <ec2-user@ip-172-31-42-51.eu-central-1.compute.internal>
2020-10-13 18:47:24 +0000

    added shutdown option

--- trainingScriptAWS.sh
+++ trainingScriptAWS.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-python train.py data/BTCUSD\=X_1d_all.csv data/BTCUSD\=X_1d_test.csv --model-name btcusd1d_30 --pretrained --episode-count 1
+python train.py data/BTCUSD\=X_1d_all.csv data/BTCUSD\=X_1d_test.csv --model-name btcusd1d_30 --pretrained --episode-count 432
 git add -A
 git commit -m "training done"
 git push origin master
+# this will need sudo
+sudo shutdown -h now
 
